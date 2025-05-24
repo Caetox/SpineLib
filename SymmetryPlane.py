c2520715b2b2f7a985561b95121382fc6dd29df1
Lara Blomenkamp <larablomenkamp@uni-koblenz.de>
2025-05-24 18:23:02 +0200

    comments symmetry plane

--- SymmetryPlane.py
+++ SymmetryPlane.py
@@ -2,94 +2,54 @@ import vtk
 import slicer
 from slicer.ScriptedLoadableModule import *
 import numpy as np
-from pathlib import Path
 from vtk.util.numpy_support import vtk_to_numpy
 import open3d as o3d
 from skspatial.objects import Plane, Points
-
-import SpineLib
 import vtk_convenience as conv
 
-
+''' Fit a symmetry plante to the geometry using ICP and mirroring'''
 def fit_symmetry_plane(geometry: vtk.vtkPolyData, numIterations: int):
 
     centerOfMass = conv.calc_center_of_mass(geometry)
     points = vtk_to_numpy(geometry.GetPoints().GetData())
     
-    # initial plane
-    p0 = calcInitalPlane(points, centerOfMass)
-    #inital_plane_node = SpineLib.SlicerTools.createMarkupsPlaneNode(p0.GetOrigin(), p0.GetNormal(), "initial plane", 100, 100)
+    # set initial plane to sagittal plane
+    p0 = vtk.vtkPlane()
+    p0.SetOrigin(centerOfMass)
+    p0.SetNormal(np.array([1.0, 0.0, 0.0]))
 
+    # mirror the geometry with the initial plane
     mirroredGeometry = mirrorWithPlane(geometry, p0)
     pFin = None
 
+    # fit the symmetry plane with n iterations
     for i in range(numIterations):
-
-        #print(f'Iteration {i+1}/{numIterations}')
         
+        # get points and calculate registration transformation with ICP
         mirroredGeometryPoints = vtk_to_numpy(mirroredGeometry.GetPoints().GetData())
         transformationMatrix = registerWithICP(mirroredGeometryPoints, points, pointToPlane=True)
-        #mirroredModel = SpineLib.SlicerTools.createModelNode(mirroredGeometry, "mirroredModel", color=[1.0, 0.0, 0.0], opacity=0.5)
-
         mirroredGeometry = conv.transform_polydata(mirroredGeometry, transformationMatrix)
-        #rigisteredModel = SpineLib.SlicerTools.createModelNode(mirroredGeometry, "registeredModel", color=[0.0, 1.0, 0.0], opacity=0.5)
         mirroredGeometryPoints = vtk_to_numpy(mirroredGeometry.GetPoints().GetData())
-        #evaluateRegistrationICP(mirroredGeometryPoints, points)
-        
+
+        # get the middle points of the original and mirrored points
         middlePoints = getMiddlePoints(points, mirroredGeometryPoints, viz=False)
+
+        # fit plane to the middle points
         p_i = fitPlaneLeastSquered(middlePoints, centerOfMass, i)
 
+        # mirror with new plane
         mirroredGeometry = mirrorWithPlane(geometry, p_i)
         pFin = p_i
-
-    # lineNode = SpineLib.SlicerTools.markupsLineNode("Right Symm", np.array(centerOfMass), np.add(np.array(centerOfMass), (np.array(pFin.GetNormal())*100)))
-    # lineNode.GetDisplayNode().SetSelectedColor(0, 1, 0)
-    # lineNode.GetDisplayNode().SetTextScale(0)
     
     return pFin
 
-
-
-def calcInitalPlane(points, centerOfMass):
-    '''
-    points: nparray. Takes numpy array and calculates the pca axes to define the initial symmetry plane (sagittal plane in our case)
-    centerOfMass: tuple. It is used to display the axes
-    returns: inital sagittal plane
-    '''
-    lineNodeNames = ['e1', 'e2', 'e3']
-    lineNodes = []
-    lineStartPos = np.asarray(centerOfMass)
-    eigenvects, eigenvals = conv.pca_eigenvectors(points)
-    #print(f'eigenvals:{eigenvals}')
-    i = 0
-    #planeNormal = eigenvects[1]
-    sorted_eigenvects = conv.closest_vector(np.concatenate((eigenvects, -eigenvects), axis=0), [1,0,0])
-    planeNormal = sorted_eigenvects[0]
-
-    # lineNode = SpineLib.SlicerTools.markupsLineNode("PCA planeNormal", lineStartPos, lineStartPos + (planeNormal*100))
-    # lineNode.GetDisplayNode().SetSelectedColor(0, 0, 1)
-    # lineNode.GetDisplayNode().SetTextScale(0)
-
-    # for lineNodeName in lineNodeNames:
-    #     e = eigenvects[i]
-    #     #we take e2 as normal to sagittal plane
-    #     lineEndPos = lineStartPos + (e*100)
-    #     lineNode = SpineLib.SlicerTools.markupsLineNode(lineNodeName, lineStartPos, lineEndPos)
-    #     lineNodes.append(lineNode)
-    #     i +=1 
-
-    plane = vtk.vtkPlane()
-    plane.SetOrigin(centerOfMass)
-    plane.SetNormal(planeNormal)
-
-    return plane
-
-
+''' Mirror the polydata with a plane.'''
 def mirrorWithPlane(polydata, plane):  
 
     normal = plane.GetNormal()
     origin = plane.GetOrigin()
     
+    # setup mirror matrix
     mirrorMatrix = vtk.vtkMatrix4x4()
     mirrorMatrix.SetElement(0, 0, 1 - 2 * normal[0] * normal[0])
     mirrorMatrix.SetElement(0, 1, - 2 * normal[0] * normal[1])
@@ -100,13 +60,13 @@ def mirrorWithPlane(polydata, plane):
     mirrorMatrix.SetElement(2, 0, - 2 * normal[0] * normal[2])
     mirrorMatrix.SetElement(2, 1, - 2 * normal[1] * normal[2])
     mirrorMatrix.SetElement(2, 2, 1 - 2 * normal[2] * normal[2])
-    
     translateWorldToPlane = [0,0,0] 
     vtk.vtkMath.Add(translateWorldToPlane, origin, translateWorldToPlane)
     translatePlaneOToWorld = [0,0,0]
     vtk.vtkMath.Add(translatePlaneOToWorld, origin, translatePlaneOToWorld)
     vtk.vtkMath.MultiplyScalar(translatePlaneOToWorld ,-1)
     
+    # create the mirror transform
     mirrorTransform = vtk.vtkTransform()
     mirrorTransform.SetMatrix(mirrorMatrix)
     mirrorTransform.PostMultiply()
@@ -117,23 +77,26 @@ def mirrorWithPlane(polydata, plane):
     mirrorFilter = vtk.vtkTransformFilter()
     mirrorFilter.SetTransform(mirrorTransform)
     mirrorFilter.SetInputData(polydata)
-    
     reverseNormalFilter = vtk.vtkReverseSense()
     reverseNormalFilter.SetInputConnection(mirrorFilter.GetOutputPort())
     reverseNormalFilter.Update()
 
     return reverseNormalFilter.GetOutput()
 
-
+''' Get the middle points of the original and mirrored points.
+    Middle points = between each original point and its mirrored counterpart.'''
 def getMiddlePoints(originalPoints, mirroredPoints, viz=True):
     
+    # randomly sample points if there are too many
     if originalPoints.shape[0] > 10000:
         inds = np.random.choice(originalPoints.shape[0], 10000)
         originalPoints = np.take(originalPoints,inds, axis=0)
         mirroredPoints = np.take(mirroredPoints,inds, axis=0)
 
+    # calculate the middle points
     middlepoints = (originalPoints + mirroredPoints)/2.0
 
+    # visualize
     if viz:
 
         vtk_points = vtk.vtkPoints()
@@ -157,20 +120,17 @@ def getMiddlePoints(originalPoints, mirroredPoints, viz=True):
     return middlepoints
 
 
-    
+''' Fit a plane to the middle points of the original and mirrored points using least squares method.'''
 def fitPlaneLeastSquered(points, centerOfMass, i):
     plane = Plane.best_fit(Points(points))
     normal = plane.normal.round(3)
-    # print(plane.normal.round(3))
-    # print(type(normal))
-
     plane = vtk.vtkPlane()
     plane.SetOrigin(centerOfMass)
     plane.SetNormal(normal)
 
     return plane
 
-
+''' Evaluate the registration of the original points and the mirrored points.'''
 def evaluateRegistrationICP(originalPoints, mirroredPoints):
     source = o3d.geometry.PointCloud()
     source.points = o3d.utility.Vector3dVector(originalPoints)
@@ -179,10 +139,9 @@ def evaluateRegistrationICP(originalPoints, mirroredPoints):
     threshold = 10000
     evaluation = o3d.pipelines.registration.evaluate_registration(source, target, threshold)
 
-    #print(f'evaluation.fitness={evaluation.fitness}, evaluation.inlier_rmse={evaluation.inlier_rmse}')
     return evaluation.fitness, evaluation.inlier_rmse
 
-
+''' Register the original points and the mirrored points with ICP.'''
 def registerWithICP(originalPoints, mirroredPoints, pointToPlane=False):
     threshold = 100000 #mm
     trans_init = np.identity(4)
